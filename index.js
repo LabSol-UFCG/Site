3b025c120a8f3443819b3183c905a0f9701032ab
ZeDaniel06 <silvadan752@gmail.com>
2024-08-28 17:58:18 -0300

    Tela Index com visual atualizado

---
+++ index.js
@@ -0,0 +1,11 @@
+//Código para adaptar certos aspectos do site de acordo com o tamanho da tela.
+// window.addEventListener('resize', function() {
+//     const width = window.innerWidth;
+//     if (width <= 768) {
+//         // Código para dispositivos móveis
+//         console.log('Modo móvel ativado');
+//     } else {
+//         // Código para desktops
+//         console.log('Modo desktop ativado');
+//     }
+// });
